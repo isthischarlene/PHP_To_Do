0f085203ba0739d01741e7381111a8c9f29f1eff
Charlene Mophethe <mophethec@gmail.com>
2019-04-07 18:16:30 +0200

    Fixed issues. List items now showing. Can strikethrough list items and remove strikethrough

--- to_do_list.php
+++ to_do_list.php
@@ -7,8 +7,10 @@ session_start();
     <head>
         <title>To Do List Challenge</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     </head>
     <body>
         <h1>To Do List</h1>
@@ -18,25 +20,19 @@ session_start();
         </form>
         
         <?php
-            if($_POST) {
-                echo "<ul>";
-                foreach($_SESSION['list-items'] as $items) {
-                "li".$items."</li>";
-                }
-                echo "</ul>";
-            }
-
-
             if(isset($_POST['addedItems'])){
                 if(!(isset($_SESSION['list-items']))){
                     $_SESSION['list-items'] = array();
-                    $_SESSION['list-items'][] = $_POST['list-items'];
-                    var_dump($_SESSION['list-items']);
+                    $_SESSION['list-items'][] = $_POST['addedItems'];
                 }else {
                     $_SESSION['list-items'][]= $_POST['addedItems'];
-                    var_dump($_SESSION['list-items']);
                 }
             }
+                echo "<ul>";
+                foreach($_SESSION['list-items'] as $item) {
+                 echo "<li>".$item."</li>";
+                }
+                 echo "</ul>";
         ?>
         
         <script>
@@ -56,6 +52,8 @@ session_start();
         });
 
         </script>
-
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
 </html>
