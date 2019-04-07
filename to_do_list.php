7aab12c1c59e07ffe2d72915939f75dc56e052de
Charlene Mophethe <mophethec@gmail.com>
2019-04-07 15:03:48 +0200

    Added functionality so that list items are added to an array but the list items not showing. Will change for next commit.

--- to_do_list.php
+++ to_do_list.php
@@ -8,27 +8,53 @@ session_start();
         <title>To Do List Challenge</title>
         <meta charset="utf-8">
         <meta name="viewport" content="initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
         <h1>To Do List</h1>
         <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
-            <input type="text" name="list-items"> 
-            <input type="submit" name="add"value="Add">
+            <input type="text" name="addedItems"> 
+            <input type="submit" value="Add">
         </form>
         
-
         <?php
             if($_POST) {
-                echo "<ul><li>".$_POST['list-items']."</li></ul><br>";
+                echo "<ul>";
+                foreach($_SESSION['list-items'] as $items) {
+                "li".$items."</li>";
+                }
+                echo "</ul>";
+            }
+
+
+            if(isset($_POST['addedItems'])){
+                if(!(isset($_SESSION['list-items']))){
+                    $_SESSION['list-items'] = array();
+                    $_SESSION['list-items'][] = $_POST['list-items'];
+                    var_dump($_SESSION['list-items']);
+                }else {
+                    $_SESSION['list-items'][]= $_POST['addedItems'];
+                    var_dump($_SESSION['list-items']);
+                }
             }
         ?>
         
         <script>
-            $(document).ready(function (){
+           $(document).ready(function (){
+            var tickedItem = 0;
+            $("li").click(function (){
+            if(tickedItem == 0) {
+                $(this).css("text-decoration", "line-through");
+                tickedItem = 1;
+                console.log(tickedItem);
+            } else {
+                    $(this).css("text-decoration", "none");
+                    tickedItem = 0;
+                    console.log(tickedItem);
+                };
+            });
+        });
 
-            })
         </script>
 
     </body>
